fc8d1b1bc4ba42b39af5aed117728c2d505a190c
William0987 <williamwsianto@gmail.com>
2023-09-06 10:52:40 -0400

    finish quick notes lab

--- src/utilities/notes-api.js
+++ src/utilities/notes-api.js
@@ -2,7 +2,7 @@ import sendRequest from "./send-request";
 const BASE_URL = '/api/notes';
 
 export function create(newNote) {
-    return sendRequest(`${BASE_URL}/create`, 'POST', newNote);
+    return sendRequest(BASE_URL, 'POST', newNote);
 }
 
 export function getAll() {
